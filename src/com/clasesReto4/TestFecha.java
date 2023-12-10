9ae0e56af7493eabf72265229bff14eebcbbb0ba
cristian-simba <davidsimba200@gmail.com>
2023-12-10 09:52:09 -0500

    encapsulamiento del reto 4

---
+++ src/com/clasesReto4/TestFecha.java
@@ -0,0 +1,13 @@
+package com.clasesReto4;
+
+public class TestFecha {
+	public static void main(String[] args) {
+		Fecha fecha1 = new Fecha();
+		fecha1.setAnio(2002);
+		System.out.println(fecha1.getAnio());	
+		fecha1.setMes(10);
+		System.out.println(fecha1.getMes());	
+		fecha1.setDia(20);
+		System.out.println(fecha1.getDia());	
+	}
+}
